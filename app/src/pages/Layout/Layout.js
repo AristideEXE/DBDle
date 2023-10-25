f687242b0521ea8ea7818f61c2c8d31db66a1182
AristideEXE <aristidepr@gmail.com>
2023-10-25 19:28:59 +0200

    Change le layout de la page

--- app/src/pages/Layout/Layout.js
+++ app/src/pages/Layout/Layout.js
@@ -6,11 +6,13 @@ import * as styles from "./Layout.css";
 const Layout = () => {
   return (
     <>
-      <div className="backgroundImage h-screen">
+      <div className="backgroundImage h-screen overflow-y-hidden">
         <Header />
-        <Outlet />
+        <div className="flex items-center justify-center w-full h-full pb-32">
+          <Outlet />
+        </div>
+        <Footer />
       </div>
-      <Footer />
     </>
   );
 };
