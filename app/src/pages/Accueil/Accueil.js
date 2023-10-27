43a16d902f5addf6cbc9793c6e51129a2f64e9bf
AristideEXE <aristidepr@gmail.com>
2023-10-27 13:16:27 +0200

    Ajoute une bordure aux boutons pour correspondre aux boutons de DBD

--- app/src/pages/Accueil/Accueil.js
+++ app/src/pages/Accueil/Accueil.js
@@ -4,7 +4,7 @@ import { survivantPath, tueurPath } from "../../routes";
 
 const Accueil = () => {
   return (
-    <div className="w-2/5 flex flex-col space-y-10">
+    <div className="w-2/5 flex flex-col space-y-8">
       <Link to={survivantPath}>
         <Button className="w-full">survivant</Button>
       </Link>
